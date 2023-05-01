fa254fc4c772ff1a272900cf11d790a2da314064
Marija Banic <marija.banic146@gmail.com>
2023-05-02 00:20:09 +0200

    Improvements

--- app/main3.py
+++ app/main3.py
@@ -6,10 +6,10 @@ matplotlib.use('Agg')
 app = Flask(__name__, template_folder='.')
 
 
-def generate_plot():
+def generate_charts(filename):
     x_data = []
     y_data = []
-    with open('data.txt', 'r') as f:
+    with open(filename, 'r') as f:
         for line in f:
             columns = line.split(",")
             x_data.append(columns[0])
@@ -19,18 +19,28 @@ def generate_plot():
     data = {'xOsa': x, 'yOsa': y}
     return data
 
+
 @app.route('/')
 def index():
-    data = generate_plot()
-    return render_template('index.html', data=data)
+    data2021 = generate_charts('oi2021.txt')
+    data2016 = generate_charts('oi2016.txt')
+    data2012 = generate_charts('oi2012.txt')
+    return render_template('index.html', data2021=data2021, data2016=data2016, data2012=data2012)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
+@app.route('/aboutApp')
+def about_app():
+    return render_template('aboutapp.html')
+
 
 @app.route('/facts')
-def didyouknow():
+def did_you_know():
     return render_template('didyouknow.html')
+
+
+@app.route('/aboutAuthor')
+def about_author():
+    return render_template('aboutauthor.html')
 
 
 if __name__ == '__main__':
